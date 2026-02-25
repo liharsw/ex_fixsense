9b872b813e424a9d8f05bd63c9493704d1d0c1b0
Lihar Sendhi Wijaya <liharsw@gmail.com>
2026-02-25 14:52:02 +0700

    use precision time with milliseconds

--- lib/ex_fixsense/protocol/utilities.ex
+++ lib/ex_fixsense/protocol/utilities.ex
@@ -8,23 +8,23 @@ defmodule ExFixsense.Protocol.Utilities do
   alias ExFixsense.Protocol.DateUtil
 
   @doc """
-  Generate FIX standard UTC timestamp.
+  Generate FIX standard UTC timestamp with millisecond precision.
 
-  Format: YYYYMMDD-HH:MM:SS
+  Format: YYYYMMDD-HH:MM:SS.mmm
 
   This is the format required by FIX 4.4 for SendingTime (tag 52),
   TransactTime (tag 60), and other timestamp fields.
 
-  Delegates to `ExFixsense.Protocol.DateUtil.now/0`.
+  Delegates to `ExFixsense.Protocol.DateUtil.now/1` with millisecond precision.
 
   ## Returns
-  String in format "20250103-14:30:45"
+  String in format "20250103-14:30:45.123"
 
   ## Example
       iex> utc_timestamp()
-      "20250103-14:30:45"
+      "20250103-14:30:45.123"
   """
   def utc_timestamp do
-    DateUtil.now()
+    DateUtil.now(include_millis: true)
   end
 end
